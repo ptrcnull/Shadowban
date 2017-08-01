58ea758917681d70cc0f3e5b5ecfbc83796eb451
Bjornskjald <github@bjorn.ml>
2017-08-01 22:32:09 +0200

    0.2.0
    added show command

--- src/ml/bjorn/shadowban/CommandHandler.java
+++ src/ml/bjorn/shadowban/CommandHandler.java
@@ -5,11 +5,14 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 
+import java.time.Instant;
+
 public class CommandHandler implements CommandExecutor {
     private Main plugin;
-
+    private FileConfiguration config;
     CommandHandler(Main instance) {
         plugin = instance;
+        config = plugin.getConfig();
     }
 
     @Override
@@ -19,11 +22,12 @@ public class CommandHandler implements CommandExecutor {
             return true;
         }
         switch(arguments[0]){
-            case "help":    help(sender);   break;
+            case "help":    help(sender); break;
             case "silent":  silent(sender); break;
             case "reload":  plugin.reloadConfig(); break;
-            case "version": version();      break;
-            default:        help(sender);   break;
+            case "version": version(); break;
+            case "show":    show(sender, arguments); break;
+            default:        help(sender); break;
         }
         return true;
     }
@@ -49,7 +53,6 @@ public class CommandHandler implements CommandExecutor {
         }
     }
     private void silent(CommandSender sender) {
-        FileConfiguration config = plugin.getConfig();
         String path = "silent." + sender.getName();
         if(config.isSet(path) && config.getBoolean(path)) {
             config.set(path, false);
@@ -62,5 +65,50 @@ public class CommandHandler implements CommandExecutor {
     }
     private void version() {
         sender.sendMessage("§bShadowban v0.1.0 enabled");
+    }
+    private void show(CommandSender sender, String[] args) {
+        String person = args.length < 2 ? sender.getName() : args[1];
+        boolean found = false;
+        if(config.contains("mute." + person)){
+            long muteEnd = config.getLong("mute." + person + ".end");
+            if(muteEnd > Instant.now().getEpochSecond()){
+                sender.sendMessage("§dMute za: §f" + config.getString("mute." + person + ".reason"));
+                sender.sendMessage("§dNadany przez: §f" + config.getString("mute." + person + ".by"));
+                sender.sendMessage("§dKoniec: §f" + config.getString("mute." + person + ".end"));
+            } else {
+                config.set("mute." + person, null);
+            }
+            found = true;
+        }
+        if(config.contains("ban." + person)){
+            long banEnd = config.getLong("ban." + person + ".end");
+            if(banEnd > Instant.now().getEpochSecond()){
+                sender.sendMessage("§dBan za: §f" + config.getString("ban." + person + ".reason"));
+                sender.sendMessage("§dNadany przez: §f" + config.getString("ban." + person + ".by"));
+                sender.sendMessage("§dKoniec: §f" + config.getString("ban." + person + ".end"));
+            } else {
+                config.set("ban." + person, null);
+            }
+            found = true;
+        }
+        if(config.contains("jail." + person)){
+            long jailEnd = config.getLong("jail." + person + ".end");
+            if(jailEnd > Instant.now().getEpochSecond()){
+                sender.sendMessage("§dJail za: §f" + config.getString("jail." + person + ".reason"));
+                sender.sendMessage("§dNadany przez: §f" + config.getString("jail." + person + ".by"));
+                sender.sendMessage("§dKoniec: §f" + config.getString("jail." + person + ".end"));
+            } else {
+                config.set("jail." + person, null);
+            }
+            found = true;
+        }
+        if(!found){
+            if(person.equals(sender.getName())){
+                sender.sendMessage("§aBrak kar na twoim koncie");
+            } else {
+                sender.sendMessage("§aBrak kar na koncie gracza " + person);
+            }
+        }
+
     }
 }
