610f38f54e7c3d1467c51dd41a936178ab651f40
Bjornskjald <github@bjorn.ml>
2017-09-03 22:31:33 +0200

    converted SubCommand to an abstract class
    added plugin/config as protected
    added lang/langf(ormat) methods

--- src/main/java/ml/bjorn/shadowban/SubCommand.java
+++ src/main/java/ml/bjorn/shadowban/SubCommand.java
@@ -1,9 +1,15 @@
 package ml.bjorn.shadowban;
 
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 
-public interface SubCommand {
-    int getMinArgs();
+public abstract class SubCommand {
+    public abstract int getMinArgs();
+    public abstract void handle(CommandSender sender, String[] args);
 
-    boolean handle(CommandSender sender, String[] args);
+    protected Main plugin = Main.plugin;
+    protected FileConfiguration config = Main.config;
+
+    protected String lang(String path) { return Main.lang.getString(path); }
+    protected String langf(String path, String... args) { return String.format(lang(path), (Object[]) args); }
 }
